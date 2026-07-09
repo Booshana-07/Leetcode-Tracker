904259c96317ae904711d474ae1974378110e5a9
Booshana-07 <booshanaarul@gmail.com>
2026-07-09 15:11:08 +0530

    Create 87-ScrambleString

---
+++ 87-ScrambleString/87-ScrambleString.java
@@ -0,0 +1,64 @@
+// Last updated: 7/9/2026, 3:11:07 PM
+import java.util.*;
+
+class Solution {
+
+    HashMap<String, Boolean> memo = new HashMap<>();
+
+    public boolean isScramble(String s1, String s2) {
+
+        if (s1.equals(s2))
+            return true;
+
+        String key = s1 + "#" + s2;
+
+        if (memo.containsKey(key))
+            return memo.get(key);
+
+        if (!sameCharacters(s1, s2)) {
+            memo.put(key, false);
+            return false;
+        }
+
+        int n = s1.length();
+
+        for (int i = 1; i < n; i++) {
+
+            // Without swapping
+            if (isScramble(s1.substring(0, i), s2.substring(0, i))
+                    && isScramble(s1.substring(i), s2.substring(i))) {
+
+                memo.put(key, true);
+                return true;
+            }
+
+            // With swapping
+            if (isScramble(s1.substring(0, i), s2.substring(n - i))
+                    && isScramble(s1.substring(i), s2.substring(0, n - i))) {
+
+                memo.put(key, true);
+                return true;
+            }
+        }
+
+        memo.put(key, false);
+        return false;
+    }
+
+    private boolean sameCharacters(String a, String b) {
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < a.length(); i++) {
+            count[a.charAt(i) - 'a']++;
+            count[b.charAt(i) - 'a']--;
+        }
+
+        for (int x : count) {
+            if (x != 0)
+                return false;
+        }
+
+        return true;
+    }
+}
