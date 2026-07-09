1e9d3e91d31c46d76d77d3c5f98a56a43ce78be8
Booshana-07 <booshanaarul@gmail.com>
2026-07-09 15:08:48 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 3:08:47 PM
+class Solution {
+    public int search(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length-1;
+        while(i <= j){
+            int mid = i + (j - i) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                i = mid + 1;
+            }else{
+                j = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
