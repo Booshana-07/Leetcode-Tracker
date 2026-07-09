bd2c30557ec8a60b8c4783d8318ab3c9f22aeef9
Booshana-07 <booshanaarul@gmail.com>
2026-07-09 15:11:30 +0530

    Create 58-LengthOfLastWord

---
+++ 58-LengthOfLastWord/58-LengthOfLastWord.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 3:11:28 PM
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();  
+        
+        int count = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                count++;
+            } else {
+                break;
+            }
+        }
+
+        return count;
+    }
+}
