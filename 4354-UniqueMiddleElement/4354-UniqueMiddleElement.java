c9e7f62f4b84af66651e488db19a379334e99575
Booshana-07 <booshanaarul@gmail.com>
2026-07-09 15:07:08 +0530

    Create 4354-UniqueMiddleElement

---
+++ 4354-UniqueMiddleElement/4354-UniqueMiddleElement.java
@@ -0,0 +1,13 @@
+// Last updated: 7/9/2026, 3:07:07 PM
+class Solution {
+    public boolean isMiddleElementUnique(int[] nums) {
+        int mid = nums[nums.length/2];
+        int count = 0;
+        for(int num : nums){
+            if(num == mid){
+                count++;
+            }
+        }
+        return count == 1;
+    }
+}
