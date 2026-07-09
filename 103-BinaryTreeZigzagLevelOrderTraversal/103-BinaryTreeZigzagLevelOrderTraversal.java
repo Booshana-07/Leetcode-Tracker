243753dd0113283aac49f1a38de7f91c076edd66
Booshana-07 <booshanaarul@gmail.com>
2026-07-09 15:11:01 +0530

    Create 103-BinaryTreeZigzagLevelOrderTraversal

---
+++ 103-BinaryTreeZigzagLevelOrderTraversal/103-BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,43 @@
+// Last updated: 7/9/2026, 3:11:00 PM
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        boolean leftToRight = true;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+            if (!leftToRight) {
+                Collections.reverse(level);
+            }
+
+            result.add(level);
+            leftToRight = !leftToRight;
+        }
+        return result;
+    }
+}
